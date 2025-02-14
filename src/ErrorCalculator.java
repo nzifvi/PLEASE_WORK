8576d2b9ffba651c48e7f641e85fe2d2c12ee340
nzifvi <benjaminaosbourne@outlook.com>
2025-02-14 22:56:12 +0100

    Began to implement the ErrorCalculator class which is responsible for calculating errors and saving them to the correct file. Removed the Connection class due to it having no use.

---
+++ src/ErrorCalculator.java
@@ -0,0 +1,21 @@
+public class ErrorCalculator {
+    Layer[] layers;
+
+    public ErrorCalculator(Layer[] layers) {
+        this.layers = layers;
+    }
+
+    public void computeGlobalError(){
+        for(int i = 0; i < layers.length; i++){
+
+        }
+    }
+
+    private void writeToFile(double[][] arr, final string filePath){
+
+    }
+
+    private void writeToFile(double[][] arr, final String filePath){
+
+    }
+}
